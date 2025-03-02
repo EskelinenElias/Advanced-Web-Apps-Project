8cf85ff1ee5afe01d491a0fcf7d24e58bbce4d38
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-03-02 23:50:06 +0200

    Improvements to client side

--- client/src/components/SideMenuMobile.tsx
+++ client/src/components/SideMenuMobile.tsx
@@ -1,14 +1,18 @@
 // Source: https://github.com/mui/material-ui/tree/v6.4.6/docs/data/material/getting-started/templates/dashboard
 
+import { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import { List, ListItem, Link } from '@mui/material'; 
 import Drawer, { drawerClasses } from '@mui/material/Drawer';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import {  useNavigate } from 'react-router-dom';
+import {  useNavigate, Link as RouterLink } from 'react-router-dom';
 import getUsername from '../auth/getUsername';
+import IBoard from '../interfaces/board';
+import verifyToken from '../auth/verifyToken';
 
 interface SideMenuMobileProps {
   open: boolean | undefined;
@@ -19,10 +23,48 @@ function SideMenuMobile({ open, toggleDrawer }: SideMenuMobileProps) {
   const navigate = useNavigate();
   const username = getUsername(); 
   
+  const token = localStorage.getItem('token'); 
+
+  const [boards, setBoards] = useState<IBoard[]>([]);
+  
+  // Load boards
+  useEffect(() => {
+    const fetchBoards = async () => {
+      if (!token) return; 
+      try {
+
+        // Fetch user boards
+        const response = await fetch(`/api/user/boards`, {
+          method: "GET",
+          headers: { 
+            'Content-Type': "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }); 
+        
+        // Check response
+        if (!response.ok) throw new Error("Failed to fetch boards"); 
+        
+        // Parse and return data
+        const data = await response.json(); 
+        setBoards(data.boards);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    
+    fetchBoards();
+  }, [token]);
   // Function to logout
   function logout() {
     localStorage.removeItem('token')
     navigate('/login')
+  }  
+  
+  function handleBoardClick(board: IBoard) {
+    if (board) {
+      navigate(`/boards/${board._id}`); 
+    }
   }
   
   return (
@@ -50,6 +92,34 @@ function SideMenuMobile({ open, toggleDrawer }: SideMenuMobileProps) {
             direction="row"
             sx={{ gap: 1, alignItems: 'center', flexGrow: 1, p: 1 }}
           >
+            <Typography variant="h3">
+              CardBoard
+            </Typography>
+          </Stack>
+        </Stack>
+        <Divider />
+        <Stack spacing={2} flexGrow={1} sx={{ p: 2 }}>
+        { verifyToken() && (
+          <>
+            <Typography variant="h4"> 
+              Boards: 
+            </Typography>
+            <List> 
+              {boards.map(board => (
+                <ListItem><Link variant="body1" component={RouterLink} to={`/boards/${board._id}`}>
+                  {board.name}
+                </Link></ListItem>
+              ))}
+            </List>
+          </>
+        )}
+        </Stack>
+        <Divider/>
+        <Stack spacing={2} sx={{ p: 2 }}>
+          <Stack
+            direction="row"
+            sx={{ gap: 1, alignItems: 'center', flexGrow: 1, p: 1 }}
+          >
             <Avatar
               sizes="small"
               alt={username}
@@ -60,19 +130,12 @@ function SideMenuMobile({ open, toggleDrawer }: SideMenuMobileProps) {
               {username}
             </Typography>
           </Stack>
-          {/* <MenuButton showBadge>
-            <NotificationsRoundedIcon />
-          </MenuButton> */}
-        </Stack>
-        <Divider />
-        <Stack sx={{ flexGrow: 1 }}>
-          <Divider />
-        </Stack>
-        {/* <CardAlert /> */}
-        <Stack sx={{ p: 2 }}>
           <Button onClick={ logout } variant="outlined" fullWidth startIcon={<LogoutRoundedIcon />}>
             Logout
           </Button>
+        </Stack>
+        <Divider/>
+        <Stack spacing={2} sx={{ p: 2 }}>
           <Typography>{"Copyright ©️ Elias Eskelinen 2025"}</Typography>
         </Stack>
       </Stack>
